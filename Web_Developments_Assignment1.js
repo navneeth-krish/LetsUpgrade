546d438ccbd14d00969f66e776858798d95c5362
Navneeth Krishna A <74096906+navneethkrishna2012@users.noreply.github.com>
2021-03-26 16:53:09 +0530

    Add files via upload

---
+++ Web_Developments_Assignment1.js
@@ -0,0 +1,25 @@
+<html>
+<body>
+
+<p1 id="ASSIGNMENT"></p1>
+
+<script1>
+var cars = ["BENZ", "BMW", "AUDI"];
+document.getElementByTagName("ASSIGNMENT").innerHTML = cars;
+</script1>
+
+<script2>
+var cars = ["BENZ", "BMW", "AUDI"];
+document.getElementByClassName("ASSIGNMENT").innerHTML = cars;
+</script2>
+
+<script3>
+var cars = ["BENZ", "BMW", "AUDI"];
+document.getElementById("ASSIGNMENT").innerHTML = cars;
+</script3>
+
+<p2 var cars = getElementsByClassName('classname');
+cars[index].innerText="BUS";
+
+</body>
+</html>
